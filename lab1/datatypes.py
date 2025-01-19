7df195e51048bae97b8ede7df4c12a7e60d9d44a
Ayau <aauzanahmet@gmail.com>
2025-01-19 14:37:01 +0600

    firstcommit

---
+++ lab1/datatypes.py
@@ -0,0 +1,36 @@
+#Exercise 1
+x = 5
+print(type(x))
+
+#result=int
+
+#Exercise 2
+x = "Hello World"
+print(type(x))
+#result=str
+
+#Exercise 3
+x = 20.5
+print(type(x))
+
+#result=float
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#result= list
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#result=tuple
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#result=dict
+
+x = True
+print(type(x))
+
+#result=bool
---
+++ lab1/variables.py
@@ -0,0 +1,35 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+#EXAMPLES
+
+carname= "Volvo"#Exercise 1
+
+x=50 #Exercise 2
+#Exercise 3
+x=5
+y=10
+print(x+y)
+
+
+#Exercise 4
+x=5
+y=10
+z=x+y
+print(z)
+
+
+#Exercise 5
+x,y,z= "Orange", "Banana", "Cherry"
+#Exercise 6
+x=y=z="Orange"
+
+
+#Exercise 7
+def myfunc():
+    global x
+    x = "fantastic"
